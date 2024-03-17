00b4f40f36ffbf375c10360dd6f0c575c20e175c
or ganon <organon333@Gmail.com>
2024-03-17 19:40:05 +0200

    commit

---
+++ src/ClassTasks/ForEach.java
@@ -0,0 +1,35 @@
+package ClassTasks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ForEach {
+    public static void main(String[] args) {
+//        //Array loop
+//        char[] letters = {'A','B','C','D','F'};
+//        for (int i = 0; i < letters.length; i++) {
+//            System.out.println(letters[i]);
+//
+////        }
+//        //for-each loop
+////        int[] numbers  = {1,2,3,4,5};
+////        for (int i : numbers) {
+////            System.out.println(i);
+//
+//        }
+
+        //foreach loop
+        List<String> names = new ArrayList<String>();
+        names.add("tal-cock");
+        names.add("Or");
+        names.add("Ofir");
+        names.add("Julie");
+        names.add("Rotem");
+        names.add("Anna");
+        names.add("Ortal");
+
+
+
+
+    }
+}
